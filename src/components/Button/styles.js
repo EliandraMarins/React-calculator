57094e2c35003561148604d41f88a9c24884558c
Eliandra Marins <eliandramarins17@gmail.com>
2022-11-14 17:07:22 +0100

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
+padding: 25px;
+border: 1px solid #CDCDCD;
+background-color: #7522C3;
+color: #FFF;
+font-size: 24px;
+font-weight: 700;
+flex: 1;
+transition: .5s;
+
+
+&:hover{
+    opacity: 0.6;
+}
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+width: 100%;
+height: 75px;
+background-color: black;
+
+display: flex;
+align-items: center;
+justify-content: flex-end;
+
+font-size: 24px;
+font-family: 'Roboto'; 
+
+
+
+input {
+    width: 100%;
+    height: 75px;
+    background-color: #353030;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    border: 2px solid black;
+    border-radius: 20px;
+    
+
+    font-size: 24px;
+    font-family: 'Roboto'; 
+    color: #FFF;
+    
+}
+`
